4d55a442380fd0cc26a21e12f9b998d79a7d290c
Heona <heonaliu@gmail.com>
2026-03-22 01:40:28 -0400

    import react-router-dom for paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+// PAGES
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Domain1 from "./pages/Domain1";
+import Domain2 from "./pages/Domain2";
+import Domain3 from "./pages/Domain3";
+import Domain4 from "./pages/Domain4";
+import Domain5 from "./pages/Domain5";
+
+
+
 export default function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-500">
-      Tailwind is working 🚀
-    </h1>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/domain1" element={<Domain1 />} />
+          <Route path="/domain2" element={<Domain2 />} />
+          <Route path="/domain3" element={<Domain3 />} />
+          <Route path="/domain4" element={<Domain4 />} />
+          <Route path="/domain5" element={<Domain5 />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
