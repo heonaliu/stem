5ba1c68b83d00cc930a015bfc8825ed7b220354c
Heona <heonaliu@gmail.com>
2026-03-22 01:27:01 -0400

    add export default function to the new components

---
+++ src/components/DropdownCard.jsx
@@ -0,0 +1,7 @@
+export default function DropdownCard() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-500">
+        Dropdown Card
+    </h1>
+  );
+}
---
+++ src/components/ProtectedEvidenceCard.jsx
@@ -0,0 +1,7 @@
+export default function ProtectedEvidenceCard() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-500">
+      Protected
+    </h1>
+  );
+}
