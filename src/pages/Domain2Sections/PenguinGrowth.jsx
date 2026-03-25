a0a50693638bc77f790379ebcdd1dc4c326c4bd7
Heona <heonaliu@gmail.com>
2026-03-25 12:52:52 -0400

    formatting on save

--- src/pages/Domain2Sections/PenguinGrowth.jsx
+++ src/pages/Domain2Sections/PenguinGrowth.jsx
@@ -5,52 +5,49 @@ import { GlowCard } from "@/components/GlowCard";
 import penguin from "@/assets/public/penguin-coding.png";
 
 export default function PenguinGrowth() {
+  return (
+    <>
+      <Card>
+        <Subheading
+          title={domain2Content.penguin.title}
+          color={"text-accent-500"}
+          size="4"
+        />
+        {domain2Content.penguin.paragraphs.map((p, i) => (
+          <p key={i}>{p}</p>
+        ))}
+      </Card>
 
-      return (
-        <>
+      <div className="flex flex-wrap gap-6 items-center justify-center">
+        <div className="w-full flex justify-center md:w-auto">
+          <GlowCard image={penguin} />
+        </div>
+        <div className="w-full md:flex-1 min-w-[250px]">
           <Card>
             <Subheading
-              title={domain2Content.penguin.title}
-              color={"text-accent-500"}
-              size="4"
+              title={domain2Content.penguin.section1Title}
+              color="text-primary-500"
+              size="2"
             />
-            {domain2Content.penguin.paragraphs.map((p, i) => (
+
+            {domain2Content.penguin.section1.map((p, i) => (
               <p key={i}>{p}</p>
             ))}
           </Card>
-    
-          <div className="flex flex-wrap gap-6 items-center justify-center">
-            <div className="w-full flex justify-center md:w-auto">
-              <GlowCard
-                image={penguin}
-              />
-            </div>
-            <div className="w-full md:flex-1 min-w-[250px]">
-              <Card>
-                <Subheading
-                  title={domain2Content.penguin.section1Title}
-                  color="text-primary-500"
-                  size="2"
-                />
-    
-                {domain2Content.penguin.section1.map((p, i) => (
-                  <p key={i}>{p}</p>
-                ))}
-              </Card>
-            </div>
-          </div>
-          <div className="w-full flex flex-col gap-6 max-w-auto justify-center">
-          <Card>
-                  <Subheading
-                    title={domain2Content.penguin.section2Title}
-                    color={"text-primary-500"}
-                    size="2"
-                  />
-                  {domain2Content.penguin.section2.map((p, i) => (
-                    <p key={i}>{p}</p>
-                  ))}
-                </Card>
-          </div>
-        </>
-      );
+        </div>
+      </div>
+      <div className="w-full flex flex-col gap-6 max-w-auto justify-center">
+        <Card>
+          <Subheading
+            title={domain2Content.penguin.section2Title}
+            color={"text-primary-500"}
+            size="2"
+          />
+          {domain2Content.penguin.section2.map((p, i) => (
+            <p key={i}>{p}</p>
+          ))}
+        </Card>
+      </div>
+    </>
+  );
 }
