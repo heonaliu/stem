ed49c788809ac821a76260df0034b528b0fc42ba
Heona <heonaliu@gmail.com>
2026-03-25 11:03:38 -0400

    add glowcard and how ive grown - not imported image yet

--- src/pages/Domain2Sections/IntRobo.jsx
+++ src/pages/Domain2Sections/IntRobo.jsx
@@ -1,6 +1,7 @@
 import Card from "@/components/Card";
 import Subheading from "@/components/Subheading";
 import { domain2Content } from "@/data/domain2Content";
+import { GlowCard } from "@/components/GlowCard";
 
 export default function IntRobo() {
   return (
@@ -15,6 +16,30 @@ export default function IntRobo() {
           <p key={i}>{p}</p>
         ))}
       </Card>
+
+      <div className="flex flex-wrap gap-6 items-center justify-center">
+              <div className="w-full flex justify-center md:w-auto">
+                <GlowCard
+                  link="https://www.aspirations.org/people/heona-l/155442"
+                  
+                />
+              </div>
+              <div className="w-full md:flex-1 min-w-[250px]">
+                <Card>
+                  <Subheading
+                    title={domain2Content.intRobo.explainTitle}
+                    color="text-primary-500"
+                    size="2"
+                  />
+    
+                  <p className="italic text-sm">click the image to be redirected!</p>
+      
+                  {domain2Content.intRobo.explain.map((p, i) => (
+                    <p key={i}>{p}</p>
+                  ))}
+                </Card>
+              </div>
+            </div>
     </>
   );
 }
--- src/pages/Domain2Sections/components/IntRoboCard.jsx
+++ src/pages/Domain2Sections/components/IntRoboCard.jsx
@@ -1,45 +1,45 @@
 import Carousel from "../../components/Carousel";
-import acsl2 from "../../assets/public/acsl-2/acsl_2.jpeg";
-import acsl3 from "../../assets/public/acsl-2/acsl_3.jpeg";
-import acsl4 from "../../assets/public/acsl-2/acsl_4.jpeg";
-import acsl5 from "../../assets/public/acsl-2/acsl_5.jpeg"
-import acsl6 from "../../assets/public/acsl-2/acsl_6.jpeg";
-import acsl7 from "../../assets/public/acsl-2/acsl_7.jpeg";
-import acsl8 from "../../assets/public/acsl-2/acsl_8.jpeg"
+import int1 from "../../assets/public/intRobo/int1.png";
+import int2 from "../../assets/public/intRobo/int2.png";
+import int3 from "../../assets/public/intRobo/int3.png";
+import int4 from "../../assets/public/intRobo/int4.png"
+import int5 from "../../assets/public/intRobo/int5.png";
+import int6 from "../../assets/public/intRobo/int6.png";
+import int7 from "../../assets/public/intRobo/int7.png"
 
 const acslItems = [
   {
-    src: acsl4,
-    title: "ACSL Contest 2:",
+    src: int1,
+    title: "int Contest 2:",
     description: "Worked through competitive programming problems...",
   },
   {
-    src: acsl3,
+    src: int2,
     title: "Problem Solving",
     description: "Focused on algorithmic thinking and efficiency...",
   },
   {
-    src: acsl2,
+    src: int3,
     title: "Team Collaboration",
     description: "Collaborated with peers to solve challenges...",
   },
   {
-    src: acsl5,
+    src: int4,
     title: "Team",
     description: "Collaborated with peers to solve challenges...",
   },
   {
-    src: acsl6,
+    src: int5,
     title: "Team Collaboration",
     description: "Collaborated with peers to solve challenges...",
   },
   {
-    src: acsl7,
+    src: int6,
     title: " Collaboration",
     description: "Collaborated with peers to solve challenges...",
   },
   {
-    src: acsl8,
+    src: int7,
     title: "Team Collaboration",
     description: " with peers to solve challenges...",
   },
