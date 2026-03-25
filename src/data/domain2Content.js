f5cae9d18e6a2f59efdea016afe7e4a7c403c3cf
Heona <heonaliu@gmail.com>
2026-03-25 12:49:53 -0400

    paragraph content explaining to the domain 2

--- src/data/domain2Content.js
+++ src/data/domain2Content.js
@@ -18,15 +18,20 @@ export const domain2Content = {
     tag: "Technology",
     dept: "Math",
     time: "Sophmore",
-    comment: "xyz"
+    comment: "xyz",
   },
 
   penguin: {
     title: "Penguin Coding School",
     paragraphs: [`...`],
     section1Title: "Next Steps In Learning",
-    section1:[`..`],
+    section1: [
+      `Summer before sophomore year, interested in looking for potential side jobs, I took initiative by constantly checking Newton North’s job listings website for potential opportunities. Upon seeing the Penguin Coding School listing, I knew this would be a perfect chance to create a positive impact on STEM in my community. Searching the internet, I learned how professionals built their resumés, and built my own for the first time. `,
+      `Because I’ve never applied to a job, the process was unfamiliar and I was nervous. A few days later, I was notified to briefly study a Scratch curriculum prior to the interview. During the interview, I demonstrated my ability to perform calmly under pressure and project my voice to assert confidence.`,
+      `Additionally, there was one key moment that stood out to my interviewer. The interview held a mock class, where the interviewer pretended to be a 6 year old student. As I was speaking, the interviewer asked a question that deviated from the curriculum I was given: “What if I want to create my own characters?” `,
+      `It wasn’t a question I expected coming in, but thinking quickly, I told him “We will learn how to create it in future lessons, but for now we will stick with the characters Scratch has given us.” In this way, I was able to pull the mock class back on track, especially key to keep things on track. Currently, I constantly use this redirection in my mentoring and when working with peers to build projects.`,
+    ],
     section2Title: "One Key Moment That Changed The Way I Teach STEM:",
-    section2: [`......`]
-  }
+    section2: [`Teaching here for over 1 year, recently during a significant point in my career that changed my perspective in the way I teach STEM. That moment is beautifully captured within my reflection below.`],
+  },
 };
