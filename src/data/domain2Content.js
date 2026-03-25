d79cbcc80a47dbd2ad933ca6b23aa245e629993b
Heona <heonaliu@gmail.com>
2026-03-25 11:52:44 -0400

    add quote data here

--- src/data/domain2Content.js
+++ src/data/domain2Content.js
@@ -10,4 +10,14 @@ export const domain2Content = {
       `So, when I started drilling holes in my robot, yes, I wanted to be as precise as I could, but I also knew that there was flexibility. Similar to many of the STEM subjects, there are many methods to get to the same result and it doesn’t have to strictly be one way. This allowed me to gather resilience to keep moving forward and look at the robot build as a whole rather than solely focusing my attention on the imperfectness of one drilled hole.`,
     ],
   },
+
+  quote1: {
+    teacher: "Ms. Daley",
+    role: "Computer Programming",
+    initial: "MD",
+    tag: "Technology",
+    dept: "Math",
+    time: "Sophmore",
+    comment: "xyz"
+  }
 };
