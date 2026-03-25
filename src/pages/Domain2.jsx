4398b7086ca84bd0afe24196326b80508f993777
Heona <heonaliu@gmail.com>
2026-03-25 10:39:34 -0400

    add int robo card header

--- src/pages/Domain2.jsx
+++ src/pages/Domain2.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Quotes1 from "./Domain2Sections/components/Quotes1";
+import IntRobo from "./Domain2Sections/IntRobo";
 
 export default function Domain2() {
   return (
     <>
       <div className="flex flex-col gap-8 p-8">
         <Header domain="2" title="Significant Growth Over Time" />
+        <IntRobo />
         <Quotes1 />
       </div>
     </>
--- src/pages/Domain2Sections/IntRobo.jsx
+++ src/pages/Domain2Sections/IntRobo.jsx
@@ -1,16 +1,17 @@
 import Card from "@/components/Card";
 import Subheading from "@/components/Subheading";
+import { domain2Content } from "@/data/domain2Content";
 
 export default function IntRobo() {
   return (
     <>
       <Card>
         <Subheading
-          title={domain1Content.transcript.title}
+          title={domain2Content.intRobo.title}
           color={"text-accent-500"}
           size="4"
         />
-        {domain1Content.transcript.paragraphs.map((p, i) => (
+        {domain2Content.intRobo.paragraphs.map((p, i) => (
           <p key={i}>{p}</p>
         ))}
       </Card>
