d4f985c953564f1e3464ecdbe7366ed8e0c7dae3
Heona <heonaliu@gmail.com>
2026-03-25 11:34:46 -0400

    import images into the card - add context and info, insert pic and import component into the intRobo section

--- src/pages/Domain2Sections/IntRobo.jsx
+++ src/pages/Domain2Sections/IntRobo.jsx
@@ -2,7 +2,8 @@ import Card from "@/components/Card";
 import Subheading from "@/components/Subheading";
 import { domain2Content } from "@/data/domain2Content";
 import { GlowCard } from "@/components/GlowCard";
-import intPic from "@/assets/public/intRobo/int7.png"
+import intPic from "@/assets/public/intRobo/int7.png";
+import IntRoboCard from "./components/IntRoboCard";
 
 export default function IntRobo() {
   return (
@@ -19,28 +20,33 @@ export default function IntRobo() {
       </Card>
 
       <div className="flex flex-wrap gap-6 items-center justify-center">
-              <div className="w-full flex justify-center md:w-auto">
-                <GlowCard
-                  link="https://docs.google.com/document/d/1mfhwovyyZTbpKXVuWuJ_qqtkyQX3MBVJ/edit?usp=sharing&ouid=101330965485149050267&rtpof=true&sd=true"
-                  image={intPic}
-                />
-              </div>
-              <div className="w-full md:flex-1 min-w-[250px]">
-                <Card>
-                  <Subheading
-                    title={domain2Content.intRobo.explainTitle}
-                    color="text-primary-500"
-                    size="2"
-                  />
-    
-                  <p className="italic text-sm">click the image to be redirected to my journal & reflections!</p>
-      
-                  {domain2Content.intRobo.explain.map((p, i) => (
-                    <p key={i}>{p}</p>
-                  ))}
-                </Card>
-              </div>
-            </div>
+        <div className="w-full flex justify-center md:w-auto">
+          <GlowCard
+            link="https://docs.google.com/document/d/1mfhwovyyZTbpKXVuWuJ_qqtkyQX3MBVJ/edit?usp=sharing&ouid=101330965485149050267&rtpof=true&sd=true"
+            image={intPic}
+          />
+        </div>
+        <div className="w-full md:flex-1 min-w-[250px]">
+          <Card>
+            <Subheading
+              title={domain2Content.intRobo.explainTitle}
+              color="text-primary-500"
+              size="2"
+            />
+
+            <p className="italic text-sm">
+              click the image to be redirected to my journal & reflections!
+            </p>
+
+            {domain2Content.intRobo.explain.map((p, i) => (
+              <p key={i}>{p}</p>
+            ))}
+          </Card>
+        </div>
+      </div>
+      <div className="w-full flex flex-col gap-6 max-w-auto justify-center">
+        <IntRoboCard />
+      </div>
     </>
   );
 }
--- src/pages/Domain2Sections/components/IntRoboCard.jsx
+++ src/pages/Domain2Sections/components/IntRoboCard.jsx
@@ -1,47 +1,53 @@
-import Carousel from "../../components/Carousel";
-import int1 from "../../assets/public/intRobo/int1.png";
-import int2 from "../../assets/public/intRobo/int2.png";
-import int3 from "../../assets/public/intRobo/int3.png";
-import int4 from "../../assets/public/intRobo/int4.png"
-import int5 from "../../assets/public/intRobo/int5.png";
-import int6 from "../../assets/public/intRobo/int6.png";
-import int7 from "../../assets/public/intRobo/int7.png"
+import Carousel from "@/components/Carousel";
+import int1 from "@/assets/public/intRobo/int1.png";
+import int2 from "@/assets/public/intRobo/int2.png";
+import int3 from "@/assets/public/intRobo/int3.png";
+import int4 from "@/assets/public/intRobo/int4.png";
+import int5 from "@/assets/public/intRobo/int5.png";
+import int6 from "@/assets/public/intRobo/int6.png";
+import int7 from "@/assets/public/intRobo/int7.png";
 
 const acslItems = [
   {
+    src: int7,
+    title: "Early Stages of our Build",
+    description:
+      "This was when we first uploaded code onto the Arduino and got it to move forward.",
+  },
+  {
     src: int1,
-    title: "int Contest 2:",
-    description: "Worked through competitive programming problems...",
+    title: "Battery Holder Fix",
+    description:
+      "Our robot was assembled however, there were no suitable holes that would securely attach the battery stand. However, I found a hole on the side of the L bracket. We removed one screw from the structure in order to fit a screw through. Not only were we able to secure the battery stand onto the structure, it could turn rotate slightly, allowing us to easily take the battery in and out.",
   },
   {
     src: int2,
-    title: "Problem Solving",
-    description: "Focused on algorithmic thinking and efficiency...",
+    title: "Problem Solving: Front Wheel",
+    description:
+      "The measurements our class was given instructed us to drill holes in specific areas of the bracket. Turns out, those measurement didn't align with the front wheel we were given. Seeing this, we used several flat brackets in order to adjust the alignment. Eventually, we were able engineer our way around this issue, proving that we can find solutions when facing obstacles in our build.",
   },
   {
     src: int3,
-    title: "Team Collaboration",
-    description: "Collaborated with peers to solve challenges...",
-  },
-  {
-    src: int4,
-    title: "Team",
-    description: "Collaborated with peers to solve challenges...",
-  },
-  {
-    src: int5,
-    title: "Team Collaboration",
-    description: "Collaborated with peers to solve challenges...",
+    title: "Progress Check",
+    description:
+      "This is when we were connecting the wires with the ESC, Arduino, and battery",
   },
   {
     src: int6,
-    title: " Collaboration",
-    description: "Collaborated with peers to solve challenges...",
+    title: "Connecting the Pieces",
+    description: "This is what it looked like with all the components attached.",
   },
   {
-    src: int7,
-    title: "Team Collaboration",
-    description: " with peers to solve challenges...",
+    src: int5,
+    title: "Metal Bracket",
+    description:
+      "After connecting our wires, our Arduino was simply floating in the center without any support or base. Seeing this, I took initiative to find a bracket that could hold the Arduino. At this time, the metal cutter's blade was being replaced, so that meant I would have to cut this piece of metal by hand. After lots of persistence, I managed to cut through, contributing toward my build and helping my team.",
+  },
+  {
+    src: int4,
+    title: "Metal Bracket: Result",
+    description:
+      "After hand cutting the metal bracket, this is what our robot looked like.",
   },
 ];
 
