490b254433875dca44ef5047c1a3cc00085ca35a
Heona <heonaliu@gmail.com>
2026-03-25 11:01:48 -0400

    add growth to intRobo for 2nd domain

--- src/data/domain2Content.js
+++ src/data/domain2Content.js
@@ -1,8 +1,11 @@
 export const domain2Content = {
-    intRobo: {
-        title: "Intermediate Robotics Journal & Reflections",
-        paragraphs:[`...`]
-    }
-
-
-}
+  intRobo: {
+    title: "Intermediate Robotics Journal & Reflections",
+    paragraphs: [`...`],
+    explainTitle: "One Example of How I've Grown",
+    explain: [
+      `During this term, I learned about the different tolerance levels for models. Initially, I believed that manufacturers had preciseness that didn’t allow for tolerance. I thought if the pieces were even off slightly from measurements, the object could be considered scrapped. However, coming into Intermediate Robotics, I worked by creating a robot with about 1/16th tolerance. This made me realize that my machine doesn’t have to be perfect in order to work.`,
+      `So, when I started drilling holes in my robot, yes, I wanted to be as precise as I could, but I also knew that there was flexibility. Similar to many of the STEM subjects, there are many methods to get to the same result and it doesn’t have to strictly be one way. This allowed me to gather resilience to keep moving forward and look at the robot build as a whole rather than solely focusing my attention on the imperfectness of one drilled hole.`,
+    ],
+  },
+};
