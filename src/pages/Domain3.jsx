bfad7275269540276758f21562ecf051d5f61433
Heona <heonaliu@gmail.com>
2026-03-26 00:04:31 -0400

    add tako component

--- src/pages/Domain3.jsx
+++ src/pages/Domain3.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Hack from "./Domain3Sections/Hack";
+import Tako from "./Domain3Sections/Tako";
 
 export default function Domain3() {
   return (
     <div className="flex flex-col gap-8 py-8 pl-8 pr-8">
       <Header domain="3" title="Commitment to Personal Study" />
       <Hack />
+      <Tako />
     </div>
   );
 }
