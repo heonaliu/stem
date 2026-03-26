d8ae2ba8c824fa43b91dd257ed75d3aaa56f54fc
Heona <heonaliu@gmail.com>
2026-03-26 00:21:34 -0400

    add the button within the card

--- src/pages/Domain3Sections/Tako.jsx
+++ src/pages/Domain3Sections/Tako.jsx
@@ -4,7 +4,9 @@ import { GlowCard } from "@/components/GlowCard";
 import { domain1Content } from "@/data/domain1Content";
 import tako from "../../assets/public/takofocus.png";
 import { domain3Content } from "@/data/domain3Content";
-
+import TakoOrbit from "./components/TakoOrbit";
+import Button from "@/components/Button";
+import { ArrowUpRight } from "lucide-react";
 export default function Tako() {
   return (
     <>
@@ -33,57 +35,68 @@ export default function Tako() {
                 size="2"
               />
             </div>
+            <p className="text-sm italic">
+              click the image to be redirected to try the web app!
+            </p>
             {domain3Content.tako.section1.map((p, i) => (
               <p key={i}>{p}</p>
             ))}
+            <Button
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://drive.google.com/file/d/1gUOT7VaHvAkrhrCX34DRkAM78-ThGy1Z/view"
+          >
+            TakoFocus Summary <ArrowUpRight className="ml-2 h-4 w-4" />
+          </Button>
           </Card>
-          
+          <div className="flex-1 py-6 min-w-[250px] max-w-auto">
+            <Card>
+              <div>
+                <Subheading
+                  title={domain3Content.tako.section2Title}
+                  color="text-primary-500"
+                  size="2"
+                />
+              </div>
+              {domain3Content.tako.section2.map((p, i) => (
+                <p key={i}>{p}</p>
+              ))}
+            </Card>
+          </div>
         </div>
-        
+        <div className="w-full mt-60 mb-60 flex items-center justify-center">
+          <TakoOrbit />
+        </div>
+      </div>
+
+      <div className="flex-1 min-w-[250px] max-w-auto">
+        <Card>
+          <div>
+            <Subheading
+              title={domain3Content.tako.section3Title}
+              color="text-primary-500"
+              size="2"
+            />
+          </div>
+          {domain3Content.tako.section3.map((p, i) => (
+            <p key={i}>{p}</p>
+          ))}
+        </Card>
       </div>
       <div className="flex-1 min-w-[250px] max-w-auto">
-          <Card>
-            <div>
-              <Subheading
-                title={domain3Content.tako.section2Title}
-                color="text-primary-500"
-                size="2"
-              />
-            </div>
-            {domain3Content.tako.section2.map((p, i) => (
-              <p key={i}>{p}</p>
-            ))}
-          </Card>
-        </div>
-        <div className="flex-1 min-w-[250px] max-w-auto">
-          <Card>
-            <div>
-              <Subheading
-                title={domain3Content.tako.section3Title}
-                color="text-primary-500"
-                size="2"
-              />
-            </div>
-            {domain3Content.tako.section3.map((p, i) => (
-              <p key={i}>{p}</p>
-            ))}
-          </Card>
-        </div>
-        <div className="flex-1 min-w-[250px] max-w-auto">
-          <Card>
-            <div>
-              <Subheading
-                title={domain3Content.tako.explainTitle}
-                color="text-primary-500"
-                size="2"
-              />
-            </div>
-            {domain3Content.tako.explain.map((p, i) => (
-              <p key={i}>{p}</p>
-            ))}
-          </Card>
-          
-        </div>
+        <Card>
+          <div>
+            <Subheading
+              title={domain3Content.tako.explainTitle}
+              color="text-primary-500"
+              size="2"
+            />
+          </div>
+          {domain3Content.tako.explain.map((p, i) => (
+            <p key={i}>{p}</p>
+          ))}
+        </Card>
+      </div>
     </>
   );
 }
