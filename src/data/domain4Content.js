c4fc646a43a820bc7efb7deb5a48ba454f680c53
Heona <heonaliu@gmail.com>
2026-03-26 23:23:39 -0400

    add domain content for ncls

---
+++ src/data/domain4Content.js
@@ -0,0 +1,17 @@
+export const domain4Content = {
+  ncls: {
+    title: "Newton Chinese Language School (NCLS)",
+    purpose: [
+      `During the 2024-2025 school year, I was a Math Teaching Assistant at the Newton Chinese Language School (NCLS), working with students both in the Wednesday Afterschool program and Sunday Day School. In this role, I supported students in strengthening their mathematical foundations, guided them through problem solving, and provided individualized feedback based on their work.`,
+      `Through grading assignments and hearing students’ approaches to problem-solving, I was able to both identify areas where students needed additional support and adjust the guidance I gave them accordingly.`,
+    ],
+    explainTitle: "How This Demonstrates STEM Citizenship",
+    explain: [
+      `This experience demonstrates STEM citizenship because it grows beyond peer collaboration and involves maintaining leadership by having a direct impact on others’ learning. By teaching, mentoring, and supporting students I advocated for an inclusive and encouraging environment where they felt comfortable asking questions and developing confidence to continue building in STEM.`,
+      `Additionally, I demonstrate initiative and commitment by continuing after returning from my (4 month) exchange program, exemplifying my dedication to supporting my students and continuously fostering curiosity in their learning.`,
+      `Lastly, NCLS recognizes outstanding volunteers, volunteer leaders for exceptional work over the year. The Emerging Leader Service Award (ELSA) recognizes volunteers who meet specific service hour requirements and is offered to appreciate leaders who strive to make a difference in the Greater Boston community. It is awarded at three levels of recognition: Gold, Silver, and Bronze Award. Further details of the award can be found under Excellence. I believe this award further reflects my active participation within the community in general and dedication to work toward improving education in STEM for many.`,
+      `Overall, my role as a Math TA reflects clear commitment to STEM citizenship by fostering growth of others, helping students build a stronger foundation toward STEM.`,
+      `Currently, I am still working with NCLS every week on Fridays After School, tutoring students in math and helping them find joy in problem solving.`,
+    ],
+  },
+};
